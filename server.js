ed787dfb8ebf0825022aa0ab403039c768e055e4
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-07-13 14:46:57 +0300

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 
-
+const mockUserData = [
+    {name:"mikey", age:29},
+    {name:"Yugi", age:26},
+]
+app.get('/users', function(req, res){
+    res.jseon({
+        success: true,
+        message: 'successfully got users',
+        users: mockUserData
+    })
+})
 
 app.listen(8000, function () {
     console.log("server is running on port 8000")
